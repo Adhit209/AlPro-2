2deba67b0644cc3c8ddbb794508f20513dd7f909
Adhit209 <61929195+Adhit209@users.noreply.github.com>
2020-06-26 10:29:08 +0700

    Update tugas proyek1.cpp

--- tugas proyek1.cpp
+++ tugas proyek1.cpp
@@ -1,88 +1,98 @@
-#include <iostream>
-#include <conio.h>
-#include <string>
-using namespace std;
-int main()
-{
-   int nip[12];
-   string nama[10];
-   int jum, gol[10],jkerja[10],jlembur[10];
-   int gapok[10],upah_lembur[10],pajakgapok[10],totpajak[10],gajibersih[10],tunjangan[10];
-	
-   cout<<"Masukan Jumlah Karyawan  : ";
-   cin>>jum;
-   cout<<endl;
-	
-   for (int i=1;i<=jum;i++){
-        cout<<"KARYAWAN KE-"<<i<<endl;
-	cout<<"Masukkan Nip              : ";
-	cin>>nip[i];
-	cout<<"Masukan Nama              : ";
-	cin>>nama[i];
-	cout<<"Masukan Golongan          : ";
-	cin>>gol[i];
-	cout<<"Masukan Jam Kerja         : ";
-	cin>>jkerja[i];
-	cout<<"Masukkan Upah Lembur      : ";
-	cin>>upah_lembur[i];
-	
-	switch (gol[i]){
-		case 1 : 
-			gapok[1]=500000;
-			upah_lembur[i]=5000;
-			break;
-		case 2 :
-			gapok[2]=300000;
-			upah_lembur[i]=3000;
-			break;
-		case 3 :
-			gapok[3]=250000;
-			upah_lembur[i]=2000;
-			break;
-		case 4 :
-			gapok[4]=100000;
-			upah_lembur[i]=1500;
-			break;
-		case 5 :
-		    gapok[5]=50000;
-			upah_lembur[i]=1000;
-			break;	
-		default :
-			gapok[i]=0;
-			upah_lembur[i]=0;	
-	}
-	
-	if (jkerja[i]>12){
-	    jlembur[i]=jkerja[i]-12;	
-	}else{
-	     jlembur[i]=0;
-	}
-	
-	upah_lembur[i]=jlembur[i]*5000;
-	upah_lembur[i]=jlembur[i]*3000;
-	upah_lembur[i]=jlembur[i]*2000;
-	upah_lembur[i]=jlembur[i]*1500;
-	upah_lembur[i]=jlembur[i]*1000;
-	upah_lembur[i]=0.10*gapok[i];
-	
-	totpajak[i]=pajakgapok[i];
-	gajibersih[i]=((gapok[i]+upah_lembur[i])-totpajak[i]);
-	cout<<endl;
-	}
-	
-	cout<<"PROGRAM MENGHITUNG GAJI KARYAWAN"<<endl;
-		cout<<endl;
-	for (int i=1;i<=jum;i++){
-		
-		cout<<"     KARYAWAN KE-"<<i<<endl;
-		cout<<"Nama                 : "<<nama[i]<<endl;
-		cout<<"Gaji Pokok           : "<<gapok[i]<<endl;		
-		cout<<"Pajak Gaji Pokok     : "<<pajakgapok[i]<<endl;
-		cout<<"Total Pajak          : "<<totpajak[i]<<endl;
-		cout<<"Upah Lembur          : "<<upah_lembur[i]<<endl;
-		cout<<"Gaji Diterima        : "<<gajibersih[i]<<endl;
-		cout<<endl<<endl;	
-	}	
 
-getch();
+#include <iostream>
+using namespace std;
+int n;
+
+struct pegawai{
+	char nip[20],nama[30];
+	float gaji,gajiB,gajiT,upah,Tgaji;
+	int golongan,jam;
+};
+struct pegawai x[10];
+
+void garis(){
+	cout<<"\n--------------------------------------------\n";
+}
+
+int inputan(){
+    for(int i=0; i<n; i++){
+		cout<<"\nPegawai ke-"<<(i+1)<<endl;
+		cout<<"Nama Pegawai     : "; cin >> x[i].nama;
+		cout<<"No.Induk Pegawai : "; cin >> x[i].nip;
+		cout<<"Gaji Pokok       : "; cin >> x[i].gaji;
+		cout<<"Golongan         : "; cin >> x[i].golongan;
+		cout<<"Berapa Jam kerja : "; cin >> x[i].jam;
+	}
+	return 0;
+}
+
+void totalgaji(){
+	 for(int i=0; i<n; i++){
+		 if(x[i].golongan== 1 && x[i].jam >= 5){
+			 x[i].gajiT = 500000;
+			 x[i].upah = (5000*(x[i].jam-5));
+			 x[i].Tgaji = x[i].gajiT + x[i].upah;
+		}
+		else if(x[i].golongan== 2 && x[i].jam >= 5){
+			 x[i].gajiT = 300000;
+			 x[i].upah = (3000*(x[i].jam-5));
+			 x[i].Tgaji = x[i].gajiT + x[i].upah;
+		}
+		else if(x[i].golongan== 3 && x[i].jam >= 5){
+			 x[i].gajiT = 250000;
+			 x[i].upah = (2000*(x[i].jam-5));
+			 x[i].Tgaji = x[i].gajiT + x[i].upah;
+		}
+		else if(x[i].golongan== 4 && x[i].jam >= 5){
+			 x[i].gajiT = 100000;
+			 x[i].upah = (1500*(x[i].jam-5));
+			 x[i].Tgaji = x[i].gajiT + x[i].upah;
+		}
+		else if(x[i].golongan== 5 && x[i].jam >= 5){
+			 x[i].gajiT = 50000;
+			 x[i].upah = (1000*(x[i].jam-5));
+			 x[i].Tgaji = x[i].gajiT + x[i].upah;
+		}
+		else{
+			cout<<"\nperusahaan : 'anda bukan gologan kami'\n";
+			break;
+		}
+	}
+}
+
+void gajibersih(){
+	for(int i=0; i<n; i++){
+		float t,p;
+		t=((15*x[i].gaji)/100.0);
+		p=((10.0*(t+x[i].gaji))/100.0);
+		x[i].gajiB=((t+x[i].gaji)-p);
+	}
+}
+	
+void output(){
+	garis();
+	garis();
+	for(int i=0; i<n; i++){
+		cout<<"\npegawai ke-"<<(i+1);
+		cout<<"\nNo.Induk Pegawai : " << x[i].nip;
+		cout<<"\nNama Pegawai     : " << x[i].nama;
+		cout<<"\nGaji Pokok       : " << x[i].gaji;
+		cout<<"\nGaji Bersih      : " << x[i].gajiB;
+		cout<<"\nGaji Tetap       : " << x[i].gajiT;
+		cout<<"\nUpah Lembur      : " << x[i].upah;
+		cout<<"\nTotal Gaji       : " << x[i].Tgaji<<endl;
+	}
+}
+
+int main(){
+	cout<<"PROGRAM UNTUK MENGHITUNG GAJI BERSIH PEGAWAI";
+	garis();
+	cout<<"\nSyarat : \n1. minimal kerja 5 jam\n2. maksimal gologan yaitu 5, tiap golongan berbeda pendapatan\n\n";
+	cout<<"Masukkan Jumlah Pegawai : ";
+	cin>>n;
+	inputan();
+	gajibersih();
+	totalgaji();
+	output();
+	return 0;
 }
